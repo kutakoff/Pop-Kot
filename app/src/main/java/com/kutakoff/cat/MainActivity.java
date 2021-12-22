a9b5794dab0cffc7e1fb4526ed752714817cb836
kutakoff <72663835+kutakoff@users.noreply.github.com>
2021-12-22 13:00:57 +0300

    add code
    add code

---
+++ app/src/main/java/com/kutakoff/cat/MainActivity.java
@@ -0,0 +1,38 @@
+package com.kutakoff.cat;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Sch_catActivity.class));
+            }
+        });
+    }
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Вы действительно хотите выйти из приложения?")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                }).setNegativeButton("Нет", null).show();
+    };
+}
---
+++ app/src/main/java/com/kutakoff/cat/Sch_catActivity.java
@@ -0,0 +1,61 @@
+package com.kutakoff.cat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Sch_catActivity extends AppCompatActivity {
+    private ImageView imageclose, imageopen, click;
+    private MediaPlayer closeSound, openSound;
+    int sch = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sch_cat);
+        TextView textView = findViewById(R.id.textView);
+        Button button_back = findViewById(R.id.button_back);
+        imageclose = findViewById(R.id.image_close);
+        imageopen = findViewById(R.id.image_open);
+        click = findViewById(R.id.click);
+        closeSound = MediaPlayer.create(this, R.raw.bop);
+        openSound = MediaPlayer.create(this, R.raw.bop);
+        imageclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("" + (sch += 1));
+                imageclose.setVisibility(View.INVISIBLE);
+                imageopen.setVisibility(View.VISIBLE);
+                soundPlay(closeSound);
+                if (sch > 0) {
+                    click.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    click.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        imageopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                soundPlay(openSound);
+                textView.setText("" + (sch += 1));
+                imageclose.setVisibility(View.VISIBLE);
+                imageopen.setVisibility(View.INVISIBLE);
+            }
+        });
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+    public void soundPlay (MediaPlayer sound) {
+    sound.start();
+    }
+}
