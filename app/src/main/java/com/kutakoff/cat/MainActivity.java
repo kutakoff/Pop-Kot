4f92bcbff7b44f2991d33baa82e5dc5eda9cc3c4
Kutakov Maxim <kutakovmax@gmail.com>
2023-08-19 01:07:33 +0300

    Changed targetSdkVersion from 29 to 33. Refreshed code. Small changes.

--- app/src/main/java/com/kutakoff/cat/MainActivity.java
+++ app/src/main/java/com/kutakoff/cat/MainActivity.java
@@ -3,12 +3,9 @@ package com.kutakoff.cat;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = findViewById(R.id.button1);
+        Button buttonStart = findViewById(R.id.start);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Sch_catActivity.class));
-            }
-        });
+        buttonStart.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Sch_catActivity.class)));
     }
+
     public void onBackPressed() {
         new AlertDialog.Builder(this)
                 .setMessage("Вы действительно хотите выйти из приложения?")
                 .setCancelable(false)
-                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                }).setNegativeButton("Нет", null).show();
-    };
+                .setPositiveButton("Да", (dialog, id) -> finish()).setNegativeButton("Нет", null).show();
+    }
 }
--- app/src/main/java/com/kutakoff/cat/Sch_catActivity.java
+++ app/src/main/java/com/kutakoff/cat/Sch_catActivity.java
@@ -1,61 +1,59 @@
 package com.kutakoff.cat;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Sch_catActivity extends AppCompatActivity {
-    private ImageView imageclose, imageopen, click;
+    private ImageView imageClose, imageOpen, click;
     private MediaPlayer closeSound, openSound;
-    int sch = 0;
+    long sch = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sch_cat);
-        TextView textView = findViewById(R.id.textView);
-        Button button_back = findViewById(R.id.button_back);
-        imageclose = findViewById(R.id.image_close);
-        imageopen = findViewById(R.id.image_open);
+        TextView textCount = findViewById(R.id.textCount);
+        imageClose = findViewById(R.id.image_close);
+        imageOpen = findViewById(R.id.image_open);
         click = findViewById(R.id.click);
+
         closeSound = MediaPlayer.create(this, R.raw.pop_cat);
         openSound = MediaPlayer.create(this, R.raw.pop_cat);
-        imageclose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("" + (sch += 1));
-                imageclose.setVisibility(View.INVISIBLE);
-                imageopen.setVisibility(View.VISIBLE);
-                soundPlay(closeSound);
-                if (sch > 0) {
-                    click.setVisibility(View.INVISIBLE);
-                }
-                else {
-                    click.setVisibility(View.VISIBLE);
-                }
+
+        imageClose.setOnClickListener(v -> {
+            textCount.setText("" + (sch += 1));
+            imageClose.setVisibility(View.INVISIBLE);
+            imageOpen.setVisibility(View.VISIBLE);
+            soundPlay(closeSound);
+            if (sch > 0) {
+                click.setVisibility(View.INVISIBLE);
+            } else {
+                click.setVisibility(View.VISIBLE);
             }
         });
-        imageopen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                soundPlay(openSound);
-                textView.setText("" + (sch += 1));
-                imageclose.setVisibility(View.VISIBLE);
-                imageopen.setVisibility(View.INVISIBLE);
-            }
-        });
-        button_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
+
+        imageOpen.setOnClickListener(v -> {
+            soundPlay(openSound);
+            textCount.setText("" + (sch += 1));
+            imageClose.setVisibility(View.VISIBLE);
+            imageOpen.setVisibility(View.INVISIBLE);
         });
     }
-    public void soundPlay (MediaPlayer sound) {
-    sound.start();
+
+    public void soundPlay(MediaPlayer sound) {
+        sound.start();
+    }
+
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Вы действительно хотите выйти из приложения? Весь прогресс будет потерян.")
+                .setCancelable(false)
+                .setPositiveButton("Да", (dialog, id) -> finish()).setNegativeButton("Нет", null).show();
     }
 }
